9e364c4cfc1ab8ebda4dff42f11c55eb5b67ac81
HanWol <hannahwolfson05@gmail.com>
2023-06-27 15:30:45 -0500

    initial commits, start of sql schema, installing dependencies, creating connection/.env file

---
+++ index.js
@@ -0,0 +1,44 @@
+// WHEN I start the application:
+    // I am presented with the following options: 
+            // view all departments, 
+            // view all roles, 
+            // view all employees, 
+            // add a department, 
+            // add a role, 
+            // add an employee,
+            // update an employee role
+// WHEN I choose to view all departments
+    // I am presented with a formatted table showing:
+        // department names
+        // department ids
+// WHEN I choose to view all roles
+    // I am presented with:
+        // the job title, 
+        // role id, 
+        // the department that role belongs to
+        // the salary for that role
+// WHEN I choose to view all employees
+    // I am presented with a formatted table showing:
+        // employee data,
+        // including employee ids,
+        // first names,
+        // last names,
+        // job titles,
+        // departments,
+        // salaries,
+        // managers that the employees report to
+// WHEN I choose to add a department
+    // I am prompted to enter the name of the department and that department is added to the database
+// WHEN I choose to add a role
+    // I am prompted to enter the name, salary, and department for the role and that role is added to the database
+// WHEN I choose to add an employee
+    // I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+// WHEN I choose to update an employee role
+    // I am prompted to select an employee to update and their new role and this information is updated in the database 
+
+const inquirer = require('inquirer');
+const db = require('./config/connection');
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
